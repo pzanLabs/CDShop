37336f70095073cbc3e88e36d50e0c2ef80bc171
mvishiu11 <jakubmuszynski2103@gmail.com>
2024-10-06 11:21:16 +0200

    feat: add controller tests, add more repository tests

---
+++ CDShop.Tests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,77 @@
+﻿using CDShop.DataAccess.Repository.IRepository;
+using CDShop.Models.ViewModels;
+using CDShop.Models;
+using CDShopWeb.Areas.Admin.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace CDShop.Tests.Controllers
+{
+    public class ProductControllerTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly ProductController _controller;
+
+        public ProductControllerTests()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _controller = new ProductController(_unitOfWorkMock.Object);
+        }
+
+        [Fact]
+        public void Index_ReturnsViewResult()
+        {
+            // Act
+            var result = _controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.NotNull(viewResult);
+        }
+
+        [Fact]
+        public void Upsert_Post_ValidModel_AddsNewProduct()
+        {
+            // Arrange
+            var productVM = new ProductVM
+            {
+                Product = new Product { Id = 0, Title = "New Product" }
+            };
+
+            _unitOfWorkMock.Setup(u => u.Product.Add(It.IsAny<Product>())).Verifiable();
+            _unitOfWorkMock.Setup(u => u.Save()).Verifiable();
+
+            // Act
+            var result = _controller.Upsert(productVM);
+
+            // Assert
+            _unitOfWorkMock.Verify(u => u.Product.Add(It.IsAny<Product>()), Times.Once);
+            _unitOfWorkMock.Verify(u => u.Save(), Times.Once);
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+        }
+
+        [Fact]
+        public void Upsert_Post_ValidModel_UpdatesExistingProduct()
+        {
+            // Arrange
+            var productVM = new ProductVM
+            {
+                Product = new Product { Id = 1, Title = "Existing Product" }
+            };
+
+            _unitOfWorkMock.Setup(u => u.Product.Update(It.IsAny<Product>())).Verifiable();
+            _unitOfWorkMock.Setup(u => u.Save()).Verifiable();
+
+            // Act
+            var result = _controller.Upsert(productVM);
+
+            // Assert
+            _unitOfWorkMock.Verify(u => u.Product.Update(It.IsAny<Product>()), Times.Once);
+            _unitOfWorkMock.Verify(u => u.Save(), Times.Once);
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+        }
+    }
+}
---
+++ CDShop.Tests/Repositories/ApplicationUserRepositoryTests.cs
@@ -0,0 +1,82 @@
+﻿using CDShop.DataAccess;
+using CDShop.DataAccess.Data;
+using CDShop.DataAccess.Repository;
+using CDShop.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace CDShop.Tests.Repositories
+{
+    public class ApplicationUserRepositoryTests
+    {
+        private DbContextOptions<ApplicationDbContext> _options;
+
+        public ApplicationUserRepositoryTests()
+        {
+            // Set up the in-memory database options
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "CDShopTestDB")
+                .Options;
+        }
+
+        [Fact]
+        public void GetApplicationUserById_ShouldReturnUser_WhenUserExists()
+        {
+            // Arrange
+            var userName = "Test User";
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var company = new Company
+                {
+                    Id = 1,
+                    Name = "Test Company"
+                };
+                context.Companies.Add(company);
+
+                // Add an ApplicationUser to the in-memory database with all required properties set
+                context.ApplicationUsers.Add(new ApplicationUser
+                {
+                    Name = userName,
+                    StreetAddress = "123 Test St",
+                    City = "Testville",
+                    State = "TS",
+                    PostalCode = "12345",
+                    CompanyId = 1
+                });
+                context.SaveChanges();
+            }
+
+            // Act
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var repository = new ApplicationUserRepository(context);
+                var user = repository.GetApplicationUserByName(userName);
+
+                // Assert
+                Assert.NotNull(user);
+                Assert.Equal("Test User", user.Name);
+                Assert.Equal("123 Test St", user.StreetAddress);
+                Assert.Equal("Testville", user.City);
+                Assert.Equal("TS", user.State);
+                Assert.Equal("12345", user.PostalCode);
+            }
+        }
+
+        [Fact]
+        public void GetApplicationUserById_ShouldReturnNull_WhenUserDoesNotExist()
+        {
+            // Arrange
+            var userName = "Other user";
+
+            // Act
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var repository = new ApplicationUserRepository(context);
+                var user = repository.GetApplicationUserByName(userName);
+
+                // Assert
+                Assert.Null(user);
+            }
+        }
+    }
+}
